58167aca291cdc878ea33a832147f214f085999a
salmaayufaa <salmaayufa@gmail.com>
2023-06-25 20:41:14 +0700

    Task 3
    task

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#a7f3d0",
+        dark: "#134e4a",
+      },
+    },
+  },
+  plugins: [],
+};
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#a7f3d0",
+        dark: "#134e4a",
+      },
+    },
+  },
+  plugins: [],
+};
